88eea9bfbb8a3c1d0c671826d01495fc5178c752
urbain12 <kortenyurbain@gmail.com>
2021-05-03 12:39:06 +0200

    payment method

--- constants/icons.js
+++ constants/icons.js
@@ -6,11 +6,19 @@ const notification_white = require("../assets/icons/notification_white.png");
 const pie_chart = require("../assets/icons/pie_chart.png");
 const right_arrow = require("../assets/icons/right_arrow.png");
 const settings = require("../assets/icons/settings.png");
+const mtn = require("../assets/icons/mtn.png");
+const airtel = require("../assets/icons/airtel.png");
+const visa = require("../assets/icons/visa.png");
+const masters = require("../assets/icons/master.png");
 const star = require("../assets/icons/star.png");
 const transaction = require("../assets/icons/transaction.png");
 
 export default {
     back_arrow,
+    mtn,
+    airtel,
+    visa,
+    masters,
     home,
     line_graph,
     notification_color,
--- navigation/tabs.js
+++ navigation/tabs.js
@@ -1,141 +1,273 @@
-import React from "react";
+import React, { useState } from "react";
 import {
-    View,
-    Image,
-    TouchableOpacity,
-    Text,
-    StyleSheet
+  View,
+  Image,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  ScrollView,
+  Text,
+  StyleSheet,
 } from "react-native";
-import { createBottomTabNavigator, BottomTabBar } from "@react-navigation/bottom-tabs"
-import { LinearGradient } from 'expo-linear-gradient'
+import {
+  createBottomTabNavigator,
+  BottomTabBar,
+} from "@react-navigation/bottom-tabs";
+import { LinearGradient } from "expo-linear-gradient";
 
-import { Home } from "../screens"
-import { COLORS, FONTS, icons } from "../constants"
+import { Home } from "../screens";
+import { COLORS, FONTS, icons } from "../constants";
+import Modal from "react-native-modal";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 const TabBarCustomButton = ({ children, onPress }) => {
-    return (
-        <TouchableOpacity
-            style={{
-                top: -30,
-                justifyContent: 'center',
-                alignItems: 'center',
-                ...styles.shadow
-            }}
-            onPress={onPress}
-        >
-            <LinearGradient
-                colors={[COLORS.primary, COLORS.secondary]}
-                style={{
-                    width: 70,
-                    height: 70,
-                    borderRadius: 35
-                }}
-            >
-                {children}
-            </LinearGradient>
-        </TouchableOpacity>
-    )
-}
+  return (
+    <TouchableOpacity
+      style={{
+        top: -30,
+        justifyContent: "center",
+        alignItems: "center",
+        ...styles.shadow,
+      }}
+      onPress={onPress}
+    >
+      <LinearGradient
+        colors={[COLORS.primary, COLORS.secondary]}
+        style={{
+          width: 70,
+          height: 70,
+          borderRadius: 35,
+        }}
+      >
+        {children}
+      </LinearGradient>
+    </TouchableOpacity>
+  );
+};
 
 const Tabs = () => {
-    return (
-        <Tab.Navigator
-            tabBarOptions={{
-                showLabel: false,
-                style: {
-                    position: 'absolute',
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    elevation: 0,
-                    backgroundColor: COLORS.white,
-                    borderTopColor: "transparent",
-                    height: 100
-                }
-            }}
-        >
-            <Tab.Screen
-                name="Home"
-                component={Home}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                            <Image
-                                source={icons.home}
-                                resizeMode="contain"
-                                style={{
-                                    width: 30,
-                                    height: 30,
-                                    tintColor: focused ? COLORS.primary : COLORS.black
-                                }}
-                            />
-                            <Text style={{ color: focused ? COLORS.primary : COLORS.black, ...FONTS.body5 }}>HOME</Text>
-                        </View>
-                    )
-                }}
-            />
-            
-            <Tab.Screen
-                name="Transaction"
-                component={Home}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <Image
-                            source={icons.transaction}
-                            resizeMode="contain"
+  const [isVisible, setIsVisible] = useState(false);
+
+  const modalHandler = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          position: "absolute",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          elevation: 0,
+          backgroundColor: COLORS.white,
+          borderTopColor: "transparent",
+          height: 100,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Modal
+                animationType="slide"
+                visible={isVisible}
+                style={{ backgroundColor: "#000000AA", margin: 0 }}
+              >
+                <TouchableOpacity
+                  onPress={modalHandler}
+                  style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <TouchableWithoutFeedback>
+                    <View
+                      style={{
+                        height: "45%",
+                        width: "90%",
+                        backgroundColor: "#fff",
+                        borderRadius: 40,
+                        padding: 20,
+                      }}
+                    >
+                      <ScrollView showsVerticalScrollIndicator={false}>
+                        <Text style={{ textAlign: "center", fontSize: 25 }}>
+                          Pay with
+                        </Text>
+                        <TouchableOpacity activeOpacity={1}>
+                          <View
                             style={{
-                                width: 30,
-                                height: 30,
-                                tintColor: COLORS.white
+                              flexDirection: "row",
+                              alignContent: "center",
                             }}
-                        />
-                    ),
-                    tabBarButton: (props) => (
-                        <TabBarCustomButton
-                            {...props}
-                        />
-                    )
+                          >
+                            <TouchableOpacity>
+                              <View style={{ alignContent: "center" }}>
+                                <Image
+                                  source={icons.mtn}
+                                  resizeMode="contain"
+                                  style={{
+                                    width: 100,
+                                    height: 100,
+                                    marginLeft: 30,
+                                  }}
+                                />
+                              </View>
+                            </TouchableOpacity>
+                            <View>
+                              <TouchableOpacity>
+                                <View>
+                                  <Image
+                                    source={icons.airtel}
+                                    resizeMode="contain"
+                                    style={{
+                                      width: 100,
+                                      height: 100,
+                                      marginLeft: 20,
+                                    }}
+                                  />
+                                </View>
+                              </TouchableOpacity>
+                            </View>
+                          </View>
+                          <View style={{ flexDirection: "row" }}>
+                            <View>
+                              <TouchableOpacity>
+                                <View>
+                                  <Image
+                                    source={icons.visa}
+                                    resizeMode="contain"
+                                    style={{
+                                      width: 100,
+                                      height: 100,
+                                      marginLeft: 30,
+                                    }}
+                                  />
+                                </View>
+                              </TouchableOpacity>
+                            </View>
+                            <View>
+                              <TouchableOpacity>
+                                <View>
+                                  <Image
+                                    source={icons.masters}
+                                    resizeMode="contain"
+                                    style={{
+                                      width: 100,
+                                      height: 100,
+                                      marginLeft: 20,
+                                    }}
+                                  />
+                                </View>
+                              </TouchableOpacity>
+                            </View>
+                          </View>
+                        </TouchableOpacity>
+                      </ScrollView>
+                    </View>
+                  </TouchableWithoutFeedback>
+                </TouchableOpacity>
+              </Modal>
+              <Image
+                source={icons.home}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? COLORS.primary : COLORS.black,
                 }}
-            />
-            
-            <Tab.Screen
-                name="Settings"
-                component={Home}
-                options={{
-                    tabBarIcon: ({ focused }) => (
-                        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-                            <Image
-                                source={icons.settings}
-                                resizeMode="contain"
-                                style={{
-                                    width: 30,
-                                    height: 30,
-                                    tintColor: focused ? COLORS.primary : COLORS.black
-                                }}
-                            />
-                            <Text style={{ color: focused ? COLORS.primary : COLORS.black, ...FONTS.body5 }}>SETTINGS</Text>
-                        </View>
-                    )
+              />
+              <Text
+                style={{
+                  color: focused ? COLORS.primary : COLORS.black,
+                  ...FONTS.body5,
                 }}
-            />
-        </Tab.Navigator>
-    )
-}
+              >
+                HOME
+              </Text>
+            </View>
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Transaction"
+        component={setIsVisible}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View>
+              <TouchableOpacity
+                style={{ width: "60%", margin: "3%" }}
+                onPress={() => {
+                  setIsVisible(true);
+                }}
+              >
+                <Image
+                  source={icons.transaction}
+                  resizeMode="contain"
+                  style={{
+                    width: 30,
+                    height: 30,
+                    tintColor: COLORS.white,
+                  }}
+                />
+              </TouchableOpacity>
+            </View>
+          ),
+          tabBarButton: (props) => <TabBarCustomButton {...props} />,
+        }}
+      />
+
+      <Tab.Screen
+        name="Settings"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <View style={{ alignItems: "center", justifyContent: "center" }}>
+              <Image
+                source={icons.settings}
+                resizeMode="contain"
+                style={{
+                  width: 30,
+                  height: 30,
+                  tintColor: focused ? COLORS.primary : COLORS.black,
+                }}
+              />
+              <Text
+                style={{
+                  color: focused ? COLORS.primary : COLORS.black,
+                  ...FONTS.body5,
+                }}
+              >
+                SETTINGS
+              </Text>
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const styles = StyleSheet.create({
-    shadow: {
-        shadowColor: COLORS.primary,
-        shadowOffset: {
-            width: 0,
-            height: 10,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
+  shadow: {
+    shadowColor: COLORS.primary,
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
 
-        elevation: 5
-    }
-})
+    elevation: 5,
+  },
+});
 
 export default Tabs;
