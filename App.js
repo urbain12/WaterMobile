38ea0f831f44383c96b882708374c4ed28ab6f61
Smn-20 <rusekezasp@gmail.com>
2021-09-24 18:08:38 +0200

    changes

--- App.js
+++ App.js
@@ -28,6 +28,7 @@ import Notifications from './screens/notifications'
 import Responses from './screens/Responses';
 import paywater from './screens/paywater';
 import request from './screens/request';
+import Landing from './screens/Landing';
 
 const Stack = createStackNavigator();
 const screenOptionStyle = {
@@ -210,7 +211,7 @@ const App = () => {
             screenOptions={{
               headerShown: false
             }}
-            initialRouteName={'Home'}
+            
           >
 
             <Stack.Screen
@@ -220,6 +221,10 @@ const App = () => {
             <Stack.Screen
               name="query"
               component={query}
+            />
+            <Stack.Screen
+              name="Landing"
+              component={Landing}
             />
             <Stack.Screen
               name="momo"
